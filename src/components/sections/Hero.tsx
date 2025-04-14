94f1c6d93cd90efda72f9fc6349c743e5e363ad4
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-04-14 17:42:51 +0300

    form UI created

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,4 +1,5 @@
 import { Container } from "../shared/Container";
+import { Paragraph } from "../shared/Paragraph";
 
 export const Hero = () => {
     return <section className="relative pt-32 lg:pt-36">
@@ -23,6 +24,47 @@ export const Hero = () => {
                         with AI{" "}
                     </span>
                 </h1>
+                <Paragraph className="mt-8">
+                    Our AI SaaS platform seamlessly integrates with your existing
+                    workflows to deliver real‑time insights, intelligent automation, and
+                    data‑driven decision-making. Experience a future where your business
+                    runs smarter, faster, and more efficiently.
+                </Paragraph>
+
+                <div className="mt-10 w-full flex max-w-md mx-auto lg:mx-0">
+                    <div className="flex sm:flex-row flex-col gap-5 w-full">
+                        <form action="#"
+                            className="py-1 pl-6 w-full pr-1 flex gap-3 items-center text-heading-3
+                        shadow-lg shadow-box-shadow border border-box-border bg-box-bg rounded-full 
+                        ease-linear focus-within:bg-body focus-with:in:border-primary">
+
+                            <span className="min-w-max pr-2 border-r border-box-border">
+                                {" "}
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    stroke="currentColor"
+                                    className="w-5 h-5"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
+                                    />
+                                </svg>
+                            </span>
+                            <input
+                                type="email"
+                                placeholder="jakedoe@email.com"
+                                className="w-full py-3 outline-none bg-transparent"
+                            />
+
+                        </form>
+                    </div>
+                </div>
 
             </div>
         </Container>
