54b7bad4dce2e0aec68c42f5399feb690b520857
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-04-15 17:34:52 +0300

    Services section and service card created

---
+++ src/components/cards/Service.tsx
@@ -0,0 +1,16 @@
+interface ServiceProps {
+    title: string;
+    description: string;
+    icon: React.ReactNode;
+}
+
+
+export const Service = ({ title, description, icon }: ServiceProps) => {
+    return (
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -1,6 +1,32 @@
+import { services } from "../../utils/services"
+import { Service } from "../cards/Service"
+import { Container } from "../shared/Container"
+import { Paragraph } from "../shared/Paragraph"
+import { Title } from "../shared/Title"
 
 export const Services = () => {
     return (
-        <section></section>
+        <section id="services">
+            <Container classname="space-y-10 md:space-y-12">
+                <div className="text-center max-w-3xl mx-auto space-y-4">
+                    <Title>Our AI Services</Title>
+                    <Paragraph>
+                        Unlock the potential of advanced machine learning, natural language
+                        processing, and predictive analytics. Our services include:
+                    </Paragraph>
+                </div>
+
+                <div>
+                    {services.map((service, key) => (
+                        <Service
+                            key={key}
+                            title={service.title}
+                            description={service.description}
+                            icon={service.icon} />
+                    ))
+                    }
+                </div>
+            </Container>
+        </section>
     )
 }
